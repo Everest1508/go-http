2a767d0a027b269adab2e8568eab845f074e5b50
Everest1508 <riteshmahale15@gmail.com>
2024-08-21 19:03:27 +0530

    added html template

--- main.go
+++ main.go
@@ -4,19 +4,31 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 type reqnres int
 
-func (m reqnres) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
+func (m reqnres) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
 
+	data := struct {
+		Method      string
+		Submissions map[string][]string
+		URL         *url.URL
+		Header      http.Header
+	}{
+		r.Method,
+		r.Form,
+		r.URL,
+		r.Header,
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	log.Println(req.Method, req.Form)
-	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+	log.Println(r.Method, r.Form)
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
 }
 
 var tpl *template.Template
